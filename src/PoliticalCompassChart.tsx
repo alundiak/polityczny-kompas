b1fcb39f32f599d843043e6d799940c7a04b5a2f
Andrii Lundiak <landike@gmail.com>
2025-05-02 01:11:23 +0200

    A few data changes

--- src/PoliticalCompassChart.tsx
+++ src/PoliticalCompassChart.tsx
@@ -72,7 +72,7 @@ const PoliticalCompassChart: React.FC = () => {
 
   // TBD
   const dummy = () => {
-    setShowPoland2025(false);
+    setShowPoland2025(true);
     setShowOtherPoland(true);
     setShowWorld(true);
   };
@@ -126,7 +126,7 @@ const PoliticalCompassChart: React.FC = () => {
             // fill="#8884d8"
             fill={getColorByType("presidential-candidate")}
             // shape="square"
-            // isAnimationActive={false}
+            isAnimationActive={false}
           >
             <LabelList
               fill="black"
@@ -216,41 +216,41 @@ const PoliticalCompassChart: React.FC = () => {
       </ScatterChart>
 
       {/* Axis labels */}
-      {/* Left label (on the left side of the chart) */}
+
       <div
         style={{
           position: "absolute",
           top: "47.5%",
-          left: "35px",
+          left: "25px",
           transform: "translateY(-50%)",
           fontWeight: "bold",
           fontSize: "20px",
           // color: "#1E90FF",
         }}
       >
+        {/* (on the left side of the chart) */}
         Left
       </div>
 
-      {/* Right label (on the right side of the chart) */}
       <div
         style={{
           position: "absolute",
           top: "47.5%",
-          right: "-15px",
+          right: "-40px",
           transform: "translateY(-50%)",
           fontWeight: "bold",
           fontSize: "20px",
           // color: "#FF4500",
         }}
       >
+        {/* on the right side of the chart) */}
         Right
       </div>
 
-      {/* Authoritarianism label (on top side of the chart) */}
       <div
         style={{
           position: "absolute",
-          top: "10px",
+          top: "0px",
           left: "54.5%",
           transform: "translateX(-50%)",
           fontWeight: "bold",
@@ -258,14 +258,14 @@ const PoliticalCompassChart: React.FC = () => {
           // color: "#8B0000",
         }}
       >
+        {/* (on top side of the chart) */}
         Authoritarianism
       </div>
 
-      {/* Libertarianism label (on bottom side of the chart) */}
       <div
         style={{
           position: "absolute",
-          bottom: "10px",
+          bottom: "5px",
           left: "54.5%",
           transform: "translateX(-50%)",
           fontWeight: "bold",
@@ -273,6 +273,7 @@ const PoliticalCompassChart: React.FC = () => {
           // color: "#32CD32",
         }}
       >
+        {/* (on bottom side of the chart) */}
         Libertarianism
       </div>
     </div>
