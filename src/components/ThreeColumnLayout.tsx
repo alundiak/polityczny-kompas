11217abb3c7b8555c03dacedc344a7ace001b18d
Andrii Lundiak <landike@gmail.com>
2026-06-29 15:48:38 +0200

    Added color for Dark Mode for 2 checkboxes

--- src/components/ThreeColumnLayout.tsx
+++ src/components/ThreeColumnLayout.tsx
@@ -63,7 +63,8 @@ const ThreeColumnLayout: React.FC = () => {
             checked={showAndrii}
             onChange={() => setShowAndrii(!showAndrii)}
           />
-          &nbsp; <span className="bg-green-100">Andrii LUNDIAK</span>
+          &nbsp;{" "}
+          <span className="bg-green-100 dark:bg-green-900">Andrii LUNDIAK</span>
         </label>
       </div>
 
@@ -88,7 +89,8 @@ const ThreeColumnLayout: React.FC = () => {
             checked={showAndriiLang}
             onChange={() => setShowAndriiLang(!showAndriiLang)}
           />
-          &nbsp; <span className="bg-red-100">Andrii LUNDIAK</span>
+          &nbsp;{" "}
+          <span className="bg-red-100 dark:bg-red-900">Andrii LUNDIAK</span>
         </label>
       </div>
       {duplicateCoordinates.length > 0 && (
